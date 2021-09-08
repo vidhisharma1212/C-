b2709647e53471d71b4f8d3fc61ca18aed57b1cc
Vidhi Sharma <vidhisharma921@gmail.com>
2021-09-09 00:55:47 +0530

    first commit

---
+++ hello.c
@@ -0,0 +1,11 @@
+a= 3;
+b= 3.9;
+c= 'Vi';
+
+// Rules-
+hello= 'First Character is an alphabet';
+_hello= 'First Character is an underscore';
+// No special char other than _
+// no commas or spaces
+// They are Case sensitive
+// Do not use keywords as variable names
