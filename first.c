c89137a4d2e4cc12cdbe1a5af7cd85a3b175b66b
Vidhi Sharma <vidhisharma921@gmail.com>
2021-09-09 14:54:05 +0530

    C course

--- first.c
+++ first.c
@@ -1,4 +1,8 @@
 # include<stdio.h>
+/*
+This is our first program in C.
+I like C Language.
+*/
 
 int main(){
     printf("Hello World");
---
+++ variables.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+/*
+This is our first program in C.
+I like C Language.
+*/
+
+int main()
+{
+    int a= 3;
+    // int b= 7.7;  not recommended, 7.7 is not an integer.
+    float b= 7.7; 
+    printf(a);
+    printf(b);
+    return 0;
+}
